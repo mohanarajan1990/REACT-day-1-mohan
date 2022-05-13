a5d77d72bc1ce2ed6859a998b4897ec840c3ffea
mohanarajan1990 <99423268+mohanarajan1990@users.noreply.github.com>
2022-05-13 21:50:00 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,53 @@
+// LET var 
+
+// function Samplevar() {
+//     var x = 5;
+//     if(x % 2 === 1) {
+//         var x = 100;
+
+//     }
+//     console.log(x);
+// }
+
+
+// function SampleLet() {
+//     var x = 5;
+//     if(x % 2 === 1) {
+//          x = 100;
+
+//     }
+//     console.log(x);
+// }
+
+// Samplevar();
+// SampleLet();
+
+//IIFE
+// const func = function () {
+//     return "something";
+
+// } ();
+
+
+// const person = {
+//     id: `31`,
+//     sleep: function () {
+
+//     }
+
+// }
+// person.sleep();
+
+
+
+function Sample (a, b) {
+    return a + b;
+}
+    console.log(Sample(5, 10));
+    const SampleFunc = (a, b) => {
+        return a + b;
+
+    }
+    console.log(SampleFunc(5, 10));
+
+
